75931ff54b76a0aaff510b872695d42c4c3145d2
Filip Krumpe <krumpefp@posteo.de>
2017-04-28 14:28:27 +0200

    Add GPL license file

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -1,3 +1,24 @@
+/*
+    The library provides a simple datastructure to access geolocated labels with an additional
+    elimination time t and a label size factor. The library provides method to query a set of such
+    labels with a bounding box and a minimum elimination time.
+    
+    Copyright (C) {2017}  {Filip Krumpe <filip.krumpe@fmi.uni-stuttgart.de}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ///
 /// A module providing a simple 2 dimensional bounding box class.
 ///
